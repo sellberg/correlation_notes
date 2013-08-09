7d52004aaeb4897b82142ea6ba69116e9cb6b43a
dermen <dermen@stanford.edu>
2013-08-09 19:53:13 -0400

    initial setup

--- setup.py
+++ setup.py
@@ -4,21 +4,17 @@
 Setup script for pypad.
 """
 
-from glob import glob
-
-
 try:
-    from setuptools import setup
-except ImportError:
     from distutils.core import setup
+except ImportError:
+    from setuptools import setup
 
 
-setup(name='pypad',
+setup(name='corrstats',
       version='0.0.1',
-      author="TJ Lane",
+      author="TJ Lane, Jonas Sellberg, Derek Mendez",
       author_email="tjlane@stanford.edu",
-      description='CSPad geometry, assembly and optimization',
-      packages=["pypad", "pypad.autogeom"],
-      package_dir={"pypad": "pypad"},
-      scripts=[s for s in glob('scripts/*') if not s.endswith('__.py')],
-      test_suite="test")
+      description='Correlation statistics calculator and X-ray intensity modifier',
+      packages=["correlation_notes"],
+      package_dir={"correlation_notes": "source"},
+      )
